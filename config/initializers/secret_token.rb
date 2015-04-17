1b5cba45635504d6c4544b75dbc71b19c7befde7
Yoshitake Komada <yoshmeister@Yoshitakes-MacBook-Air.local>
2015-04-17 15:45:35 +0900

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caloriemate::Application.config.secret_token = '57fe7e653fc9f9e3398f80f183209f726762aee06b1693fc2858e70942845598f7449db2663ca5c4706855c4839108f7a988bde804f14602940e492870393e2a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Caloriemate::Application.config.session_store :cookie_store, key: '_caloriemate_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Caloriemate::Application.config.session_store :active_record_store
